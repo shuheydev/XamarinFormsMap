ae0359745e2b0ab27f332392b907c3f2e870260b
Shuhey <shuhey.dev@gmail.com>
2020-04-23 14:08:14 +0900

    Add project files.

---
+++ XamarinFormsMap/XamarinFormsMap.Android/MyMap_Droid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms;
+using XamarinFormsMap.Droid;
+
+[assembly: Dependency(typeof(MyMap_Droid))]
+
+namespace XamarinFormsMap.Droid
+{
+    public interface IMap_Droid
+    { }
+
+
+
+    public class MyMap_Droid:IMap_Droid
+    {
+
+    }
+}
---
+++ XamarinFormsMap/XamarinFormsMap/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Xamarin.Forms;
+
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFormsMap
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+            var a = new XamarinFormsMap.MainPage();
+            a
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+
+
+    }
+}
